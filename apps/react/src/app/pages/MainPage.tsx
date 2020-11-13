6283c5e6e868bbb61467b8b3102d67a3fc8bb712
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-13 17:09:08 +0300

    Update main page and navbar

--- apps/react/src/app/pages/MainPage.tsx
+++ apps/react/src/app/pages/MainPage.tsx
@@ -1,10 +1,74 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Jumbotron, Row, ModalFooter, Col, Card } from 'react-bootstrap';
+import styled from 'styled-components';
+import { Search } from '@internship/ui';
+
+const StyledApp = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+const StyledRow = styled(Row)`
+    margin: 0px auto;
+    padding: 50px 0px 50px 0px;
+    border-bottom: 1px ridge;
+    min-height: 400px;
+`;
+
+const StyledJumbotron = styled(Jumbotron)`
+  padding: 70px 30px 70px 30px;
+  margin: 0px auto;
+  background: gray;
+  color: floralwhite;
+`;
+
+const Container = styled.div`
+  display: inline-block;
+  padding: 4.5rem;
+`;
+
+const StyledCard = styled(Card)`
+  min-height: 400px;
+  background: green;
+`;
+
 
 export const MainPage = () => {
   return (
-    <Container>
-      <h2>MainPage</h2>
-    </Container>
+    <StyledApp>
+      <StyledJumbotron>
+        <Container>
+          <Row>
+            <Col>
+              <h1>There is home page</h1>
+              <p>There is a paragraph that describes site</p>
+            </Col>
+          </Row>
+        </Container>
+
+      </StyledJumbotron>
+      <Container>
+        <StyledRow>
+          <h2>Last Reviewed Books</h2>
+        </StyledRow>
+        <StyledRow>
+          <h2>Last Reviewed Movies</h2>
+        </StyledRow>
+        <StyledRow>
+          <Col>
+            <StyledCard>
+              <h3>Most Liked Books</h3>
+            </StyledCard>
+            </Col>
+          <Col>
+            <StyledCard>
+              <h3>Most Liked Movies</h3>
+            </StyledCard>
+          </Col>
+        </StyledRow>
+      </Container>
+
+    </StyledApp>
+
   );
 };
--- libs/ui/src/lib/organisms/Navigation/Navigation.tsx
+++ libs/ui/src/lib/organisms/Navigation/Navigation.tsx
@@ -7,6 +7,9 @@ import { logoutAsync } from '@internship/store/authentication';
 import { useDispatch } from 'react-redux';
 import { Popup, PopupButton, Search } from '../../molecules';
 import { getAccessToken, getRefreshToken } from '@internship/shared/utils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faFilm, faHome } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '../../atoms/Button';
 
 export const Navigation = () => {
   const { isAuthenticated } = useAuthentication();
@@ -39,7 +42,7 @@ export const Navigation = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-sm bg-primary  navbar-dark">
+    <nav className="navbar navbar-expand-md bg-dark  navbar-dark">
       <div className="container">
         <button
           className="custom-toggler navbar-toggler"
@@ -55,66 +58,74 @@ export const Navigation = () => {
         </button>
         <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbar">
           <ul className="navbar-nav mr-auto">
-            <li className="navbar-brand">
+            <li className="navbar-brand nav-link">
               <NavLink exact to="/" className="nav-link"
                        onClick={() => dispatch({ type: '@temp/ERROR_REQUIRED', payload: null })}>
-                Home
+                <FontAwesomeIcon icon={faHome} style={{ marginRight: '10px' }} />Home
               </NavLink>
             </li>
-            <li className="nav-link">
+            <li className="navbar-brand nav-link">
               <NavLink
-                to="/about"
+                to="/book"
                 className="nav-link"
                 onClick={() => {
                   dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
                   dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
                 }}
               >
-                About
+                <FontAwesomeIcon icon={faBook} style={{ marginRight: '10px' }} />Book
               </NavLink>
             </li>
-            <li className="nav-link">
+            <li className="navbar-brand nav-link">
               <NavLink
-                to="/contact"
+                to="/movie"
                 className="nav-link"
                 onClick={() => {
                   dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
                   dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
                 }}
               >
-                Contact Us
+                <FontAwesomeIcon icon={faFilm} style={{ marginRight: '10px' }} />Movie
               </NavLink>
             </li>
             {isAuthenticated ? (
-              <NavDropdown className="nav-link" title={<FaUserAlt />} id="basic-nav-dropdown">
-                <NavLink
-                  className="dropdown-item"
-                  to="/profile"
-                  type="button"
-                  onClick={() => {
-                    dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
-                    dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
-                  }}
-                >
-                  Profile
-                </NavLink>
-                <NavDropdown.Item type="button" to={location.pathname} onClick={handleOpen}>
-                  Logout
-                </NavDropdown.Item>
-                <Popup show={show} onHide={handleClose}>
-                  Sistemden Çıkıyorsunuz Emin misiniz?
-                  <PopupButton variant="secondary" onClick={handleClose}>
-                    HAYIR
-                  </PopupButton>
-                  <PopupButton type="submit" variant="primary" onClick={handleShow}>
-                    EVET
-                  </PopupButton>
-                </Popup>
-              </NavDropdown>
+              <>
+                <NavDropdown className="nav-link" title={<FaUserAlt />} id="basic-nav-dropdown">
+                  <NavLink
+                    className="dropdown-item"
+                    to="/profile"
+                    type="button"
+                    onClick={() => {
+                      dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
+                      dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
+                    }}
+                  >
+                    Profile
+                  </NavLink>
+                  <NavDropdown.Item type="button" to={location.pathname} onClick={handleOpen}>
+                    Logout
+                  </NavDropdown.Item>
+                  <Popup show={show} onHide={handleClose}>
+                    Sistemden Çıkıyorsunuz Emin misiniz?
+                    <PopupButton variant="secondary" onClick={handleClose}>
+                      HAYIR
+                    </PopupButton>
+                    <PopupButton type="submit" variant="primary" onClick={handleShow}>
+                      EVET
+                    </PopupButton>
+                  </Popup>
+                </NavDropdown>
+                <li className="navbar-brand nav-link">
+                  <NavLink to="/becomeAnEditor">
+                    <Button variant="outline-primary">Do you want to become an editor?</Button>
+                  </NavLink>
+                </li>
+
+              </>
             ) : (
               <NavDropdown className="nav-link" title="Account" id="basic-nav-dropdown">
                 <NavLink
-                  className="dropdown-item"
+                  className="mr-auto dropdown-item"
                   to="/register"
                   onClick={() => {
                     dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
@@ -136,7 +147,6 @@ export const Navigation = () => {
               </NavDropdown>
             )}
           </ul>
-          <Search />
         </div>
       </div>
     </nav>
