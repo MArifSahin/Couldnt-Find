38c73682c999d0dfd212ae301fdeb5e2bb7ac59b
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-13 17:10:36 +0300

    Update profile page

--- apps/react/src/app/pages/Profile.tsx
+++ apps/react/src/app/pages/Profile.tsx
@@ -6,14 +6,14 @@ import { ProfileImage } from '@internship/ui';
 import { useAuthentication } from '@internship/shared/hooks';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Profile = () => {
   const [inEditMode, setInEditMode] = useState(false);
   const [inChangePassword, setInChangePassword] = useState(false);
   const [editUserInfo, setEditUserInfo] = useState(false);
   const [sessionInfo, setSessionInfo] = useState(false);
+  const [showUserInfo, setShowUserInfo] = useState(true);
   const [detail, setDetail] = useState<UserDetailResponse>();
   const { isAuthenticated } = useAuthentication();
   const history = useHistory();
@@ -36,6 +36,7 @@ export const Profile = () => {
     setInEditMode(true);
     setInChangePassword(false);
     setSessionInfo(false);
+    setShowUserInfo(false);
     dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
     dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
   };
@@ -52,37 +53,26 @@ export const Profile = () => {
       <h2>Profile Page</h2>
       <Row>
         <Col sm={6}>
+          <div className="card-header">
+            <h3>Welcome</h3>
+            <ProfileImage width="200" height="200" alt={`${detail?.username} profile picture`}
+                          image={detail?.image} />
+          </div>
           <div className="card text-center">
-            <div className="card-header">
-              <h3>Welcome</h3>
-              <ProfileImage width="200" height="200" alt={`${detail?.username} profile picture`}
-                            image={detail?.image} />
-            </div>
-            <h5>
-              <div>
-                <h4>
-                  <b className="text-black-50">User Info</b>
-                </h4>
-                <Row>
-                  <i className="text-black-50 ml-4"> UserName: {detail?.username}</i>
-                </Row>
-                <Row>
-                  <i className="text-black-50 ml-4"> Name:{detail?.name}</i>
-                </Row>
-                <Row>
-                  <i className="text-black-50 ml-4"> SurName:{detail?.lastName}</i>
-                </Row>
-                <Row>
-                  <i className="text-black-50 ml-4"> Age: {detail?.age}</i>
-                </Row>
-                <Row>
-                  <i className="text-black-50 ml-4"> Phone: {detail?.phoneNumber}</i>
-                </Row>
-                <Row>
-                  <i className="text-black-50 ml-4 "> Email: {detail?.email}</i>
-                </Row>
-              </div>
-            </h5>
+            <Button
+              className="btn  btn-success mt-2"
+              disabled={showUserInfo}
+              onClick={() => {
+                dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
+                dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
+                setShowUserInfo(true);
+                setInEditMode(false);
+                setSessionInfo(false);
+                setInChangePassword(false);
+              }}
+            >
+              My Information
+            </Button>
             <Button className="btn  btn-success mt-2" disabled={inEditMode} onClick={changeValues}>
               Edit Profile
             </Button>
@@ -92,56 +82,75 @@ export const Profile = () => {
               onClick={() => {
                 setInChangePassword(true);
                 setInEditMode(false);
+                setSessionInfo(false);
+                setShowUserInfo(false);
                 dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
                 dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
-                setSessionInfo(false);
               }}
             >
               Change Password
             </Button>
-            <Button className="btn  btn-success mt-2" disabled={sessionInfo} onClick={editSessionInfo}>
+            <Button
+              className="btn  btn-success mt-2"
+              disabled={sessionInfo}
+              onClick={() => {
+                setSessionInfo(true);
+                setInChangePassword(false);
+                setInEditMode(false);
+                setShowUserInfo(false);
+                dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
+                dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
+              }}>
               Session Info
             </Button>
           </div>
         </Col>
         <Col sm={6}>
+          {showUserInfo && (
+            <h5>
+              <div>
+                <h4>
+                  <b className="text-black-50">User Info</b>
+                </h4>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2"> Username:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.username} </i></Col>
+                </Row>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2"> Name:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.name} </i></Col>
+                </Row>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2"> Surname:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.lastName} </i></Col>
+                </Row>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2"> Age:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.age} </i></Col>
+                </Row>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2"> Phone:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.phoneNumber} </i></Col>
+                </Row>
+                <Row>
+                  <Col sm={2}><i className="text-black-50 ml-2 "> Email:</i></Col>
+                  <Col sm={3}><i className="text-black-50 ml-2"> {detail?.email} </i></Col>
+                </Row>
+              </div>
+            </h5>
+          )}
           {inEditMode && (
             <>
-              <Button
-                className="btn btn-danger"
-                disabled={!inEditMode}
-                onClick={() => {
-                  setInEditMode(false);
-                  dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
-                  dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
-                }}
-              >
-                <FontAwesomeIcon icon={faTimes} />
-              </Button>
               <EditProfile setInEditMode={setInEditMode} setEditUserInfo={setEditUserInfo} userInfo={detail} />
             </>
           )}
           {inChangePassword && (
             <>
-              <Button
-                className="btn btn-danger"
-                disabled={!inChangePassword}
-                onClick={() => {
-                  setInChangePassword(false);
-                  dispatch({ type: '@temp/ERROR_REQUIRED', payload: null });
-                  dispatch({ type: '@temp/SUCCESS_REQUIRED', payload: null });
-                }}
-              >
-                <FontAwesomeIcon icon={faTimes} />
-              </Button>
               <ChangePassword />
             </>
           )}
           {sessionInfo ? (
             <>
-              <Button className="btn btn-danger mb-3" disabled={!sessionInfo} onClick={() => setSessionInfo(false)}>
-                <FontAwesomeIcon icon={faTimes} />
-              </Button>
               <EditSession />
             </>
           ) : null}
