7383d56f7a2033d6bb649b7dfc6140b2b73cc04d
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-22 15:41:47 +0300

    Update indexing

--- apps/react/src/app/pages/index.ts
+++ apps/react/src/app/pages/index.ts
@@ -5,10 +5,10 @@ export * from './MainPage';
 export * from './OAuth2RedirectHandler';
 export * from './Profile';
 export * from './Register';
-export * from './MailActivation';
+export * from './mailActivation';
 export * from './Book';
 export * from './Movie';
-export * from './BookReviewPage';
-export * from './ForgotPassword';
+export * from './bookReviewPageComponents/BookReviewPage';
+export * from './forgotPassword';
 export * from './BecomeAnEditor';
 
