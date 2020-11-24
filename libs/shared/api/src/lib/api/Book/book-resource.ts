3bf93cde0be9833dab914f637c49039cd9c747bc
MArifSahin <mehmetarifsahin@etu.edu.tr>
2020-11-24 17:06:29 +0300

    Update getBookContent request

--- libs/shared/api/src/lib/api/Book/book-resource.ts
+++ libs/shared/api/src/lib/api/Book/book-resource.ts
@@ -8,7 +8,7 @@ export class BookResource {
 
   writeUserReview = (data: any): Promise<any> => this.axios.post('book/write-user-review', data, this.axiosRequestConfig).then((r) => r.data);
   writeEditorReview = (data: any): Promise<any> => this.axios.post('book/write-editor-review', data, this.axiosRequestConfig).then((r) => r.data);
-  getBookContent = (bookId: any): Promise<BookDetailResponse> => this.axios.get('book/?bookId='+encodeURIComponent(bookId)).then((r) => r.data);
+  getBookContent = (bookId: any): Promise<BookDetailResponse> => this.axios.get('book/'+bookId, this.axiosRequestConfig).then((r) => r.data);
   getLatestReviews = (): Promise<any> => this.axios.get('book/last-reviews', this.axiosRequestConfig).then((r) => r.data);
   getHighestRatedBooks = (): Promise<any> => this.axios.get('book/highest-rated', this.axiosRequestConfig).then((r) => r.data);
   getHighestReviewedBooks = (): Promise<any> => this.axios.get('book/highest-reviewed', this.axiosRequestConfig).then((r) => r.data);
